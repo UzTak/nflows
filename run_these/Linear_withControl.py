829281215086aac54944bbb4e8f22964b88d1186
UzTak <49874360+UzTak@users.noreply.github.com>
2022-03-31 16:36:06 +0200

    0331-2

---
+++ run_these/Linear_withControl.py
@@ -0,0 +1,397 @@
+"""
+Normalizing Flow Algorithm
+Consider the flow z = f(x), where f(x) = fN @ ... @ f1 @ f0(x0)
+i.e., x = distribution to be sampled at every iteration
+      z = target distribution = base distribution
+
+Dynamics: x_ = Ax + Bu, where u = KX + V (linear feedback law)
+A, B: Dynamics coefficients (fixed)
+K, V: Optimized Variable/parameters
+"""
+
+
+import matplotlib.pyplot as plt
+from mpl_toolkits import mplot3d
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as tf
+import sklearn.datasets as datasets
+import numpy as np
+import copy
+import random
+
+import torch
+from torch import optim
+
+from nflows.flows.base import Flow
+from nflows.distributions.normal import StandardNormal
+from nflows.transforms.base import CompositeTransform
+from linear_trans_wContr import NaiveLinear
+from nflows.transforms.permutations import ReversePermutation
+
+import global_variable as g
+
+num_layers = 5
+# MC sampling number
+M = 100
+
+g.num_iter = 15000
+g.idx = 0
+
+base_dist = StandardNormal(shape=[2])
+
+transforms = []
+for _ in range(num_layers):
+    # do we need a permulation layer for the linear flow?
+    # transforms.append(ReversePermutation(features=2))
+    transforms.append(NaiveLinear(features=2))
+transform = CompositeTransform(transforms)
+
+flow = Flow(transform, base_dist)
+optimizer = optim.Adam(flow.parameters())
+# print(list(flow.parameters()))
+
+for i in range(g.num_iter):
+    g.idx = i
+
+    """ Generating Initial Distribution (x)...  """
+    # moons
+    # x, y = datasets.make_moons(128, noise=.1)
+
+    # normal dist.
+    x, _ = datasets.make_gaussian_quantiles(mean=[0,0], cov=[5,5], n_samples=128, n_features=2, n_classes=1)
+    # x = data[:,0]
+    # y = data[:,1]
+    # plt.scatter(x,y)
+
+    # multimodal normal dist.
+    # x1, y1 = datasets.make_gaussian_quantiles(mean=[1,2], n_samples=50)
+    # x2, y2 = datasets.make_gaussian_quantiles(mean=[-1,-2], n_samples=100)
+    # x = np.concatenate([x1, x2], 0)
+
+
+    x = torch.tensor(x, dtype=torch.float32)
+    optimizer.zero_grad()
+    loss = -flow.log_prob(inputs=x).mean()
+    # print(loss)
+    loss.backward()
+    optimizer.step()
+
+    # at the final iteration
+    # flow from Normal (base) to the sampling dist.
+    if (i + 1) % g.num_iter == 0:
+        xline = torch.linspace(-3, 5, 100)
+        yline = torch.linspace(-4, 6, 100)
+        xgrid, ygrid = torch.meshgrid(xline, yline)
+        # concatenate
+        xyinput = torch.cat([xgrid.reshape(-1, 1), ygrid.reshape(-1, 1)], dim=1)
+
+        # we don't need a gradient for this, so nullify the gradient info
+        with torch.no_grad():
+            zgrid = flow.log_prob(xyinput).exp().reshape(100, 100)
+
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111)
+        #
+        # ax.set_aspect('equal', adjustable='box')
+        # plt.contourf(xgrid.numpy(), ygrid.numpy(), zgrid.numpy())
+        # plt.title('iteration {}'.format(i + 1))
+
+        # printing the parameters
+        K_list = []
+        V_list = []
+        # print("iteration", i+1)
+        para = list(flow.parameters())
+        # para = list(flow.state_dict())
+
+        j = 0
+        # print(para)
+        for a, b in para:
+            # print (a, b)
+            # K_list.append([a.data.tolist(), b.data.tolist()])
+
+            if np.mod(j,2) == 0:
+                V_list.append([a.data.tolist(), b.data.tolist()])
+            else:
+                K_list.append([a.data.tolist(), b.data.tolist()])
+            j += 1
+
+
+        # flow from the sampling dist. to the base dist. (normal)
+        x, _ = datasets.make_gaussian_quantiles(mean=[0,0], cov=[5, 5], n_samples=M, n_features=2, n_classes=1)
+        with torch.no_grad():
+            x = torch.tensor(x)
+        noise_out = flow.transform_to_noise(x.float())
+        noise_out = noise_out.detach().numpy()
+
+        # fig3 = plt.figure()
+        # ax3 = fig3.add_subplot(111)
+        # ax3.axis('equal')
+        # plt.scatter(noise_out[:,0], noise_out[:,1])
+
+A = np.matrix([[1, 0.3], [0.5, 1]])
+B = np.matrix([[0.7, 0.3], [0.1, 0.8]])
+
+
+K_list2 = [np.matrix(K_list[0])]
+K_list_ = [np.matrix(K_list[0])]
+coeff = 1
+for m in range(len(K_list)-1):
+    coeff = coeff * (A+B*(np.matrix(K_list[m])))
+    K_list2.append(K_list[m+1] * coeff)
+    K_list_.append(np.matrix(K_list[m+1]))
+
+# print(K_list_)
+
+"""
+MC simulation
+x_MC: propagation of x_{k+1} = A*x_{k} + B*K*u_{k} 
+y_MC: propagation of x_{k+1} = A*x_{k} + B*K'*u_{0}
+z_MC: utilization of Script A & B. x_{k+1} = ScrA * x0 + ScrB * ScrK * x0
+
+"""
+
+def confidence_ellipse(x, y, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+
+    Parameters
+    ----------
+    x, y : array-like, shape (n, )
+        Input data.
+
+    ax : matplotlib.axes.Axes
+        The axes object to draw the ellipse into.
+
+    n_std : float
+        The number of standard deviations to determine the ellipse's radiuses.
+
+    **kwargs
+        Forwarded to `~matplotlib.patches.Ellipse`
+
+    Returns
+    -------
+    matplotlib.patches.Ellipse
+    """
+    if x.size != y.size:
+        raise ValueError("x and y must be the same size")
+
+    cov = np.cov(x, y)
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = np.mean(x)
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = np.mean(y)
+
+    transf = tf.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+def confidence_ellipse2(cov, avg, ax, n_std=3.0, facecolor='none', **kwargs):
+    """
+    Take the covariance matrix, and return the ellipse
+    """
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    # Using a special case to obtain the eigenvalues of this
+    # two-dimensionl dataset.
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    # Calculating the stdandard deviation of x from
+    # the squareroot of the variance and multiplying
+    # with the given number of standard deviations.
+    scale_x = np.sqrt(cov[0, 0]) * n_std
+    mean_x = avg[0]
+
+    # calculating the stdandard deviation of y ...
+    scale_y = np.sqrt(cov[1, 1]) * n_std
+    mean_y = avg[1]
+
+    transf = tf.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(mean_x, mean_y)
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
+
+
+fig2 = plt.figure()
+ax21 = fig2.add_subplot(111)
+ax21.axis('equal')
+ax21.grid()
+ax21.set_title('Trajectory')
+ax21.set_xlabel('x')
+ax21.set_ylabel('y')
+
+
+fig5 = plt.figure()
+ax22 = fig5.add_subplot(211)
+ax22.grid()
+ax22.set_title('x vs time')
+ax22.set_xlabel('layer')
+ax22.set_ylabel('x')
+
+ax23 = fig5.add_subplot(212)
+ax23.grid()
+ax23.set_title('y vs time')
+ax23.set_xlabel('layer')
+ax23.set_ylabel('y')
+
+# ax24 = fig2.add_subplot(224)
+# ax24.grid()
+# ax24.set_title('u vs time')
+# ax24.set_xlabel('layer')
+# ax24.set_ylabel('u')
+
+# fig6 = plt.figure()
+# ax61 = fig6.add_subplot(111)
+# ax61.grid()
+# ax61.set_title('control vs time')
+# ax61.set_xlabel('step')
+# ax61.set_ylabel('control')
+
+
+output_MC = []
+x_log = np.zeros((M,num_layers+1))
+y_log = np.zeros((M,num_layers+1))
+unorm_log = np.zeros((M,1))
+
+time = range(num_layers+1)
+for i in range(M):
+    mu0 = np.matrix([[0], [0]])
+    Sigma0 = np.matrix([[5, 0], [0,5]])
+    # x0_MC = mu0 + np.sqrt(Sigma0) * np.random.uniform(low=-1,high=1,size=(2,1))
+    # x0_MC = [[random.gauss(0, 5)], [random.gauss(0, 5)]]
+    x0_MC = np.matrix(x[i]).T
+    x_MC = np.matrix(np.zeros((2, num_layers+1)))
+    u_MC = np.zeros((2,num_layers))
+    unorm_MC = np.zeros(num_layers)
+
+    y_MC = copy.deepcopy(x_MC)
+    # Uvec = np.matrix(np.zeros((num_layers*2,1)))
+    
+    for t in range(num_layers+1):
+        if t == 0:
+            x_MC[:, t] = x0_MC
+        else:
+            U = K_list_[t-1] * x_MC[:,t-1]
+            u_MC[:,t-1] = U.ravel()
+            unorm_MC[t-1] = np.linalg.norm(U.ravel())
+            Uy = K_list2[t-1] * x0_MC
+            x_MC[:,t] = (A + B*K_list_[t-1])*x_MC[:,t-1] + np.matrix(V_list[t-1]).T
+            y_MC[:,t] = A*y_MC[:,t-1] + B*Uy #+ np.matrix(V_list[t]).T
+
+        # Uvec[2*t,0] = Uy[0]
+        # Uvec[2*t+1,0] = Uy[1]
+    x_MC = np.asarray(x_MC)
+
+    # ax21 plots
+    # initial point
+    ax21.scatter(x_MC[0][0], x_MC[1][0], color='r', s=2, zorder=2)
+    # trajectory
+    ax21.plot(x_MC[0], x_MC[1], '-', color='lightgrey', linewidth=1, zorder=0)
+    # terminal: These three return the same points
+    ax21.scatter(x_MC[0][num_layers], x_MC[1][num_layers], color='k', s=2, zorder=2)
+    # plt.scatter(y_MC[0,-1], y_MC[1,-1], color='g', s=3)
+
+    # ax22 plots: x vs time
+    ax22.plot(time, x_MC[0], '-', color='lightgrey', linewidth=1, zorder=1)
+    x_log[i,:] = x_MC[0]
+
+    # ax23 plots: y vs time
+    ax23.plot(time, x_MC[1], '-', color='lightgrey', linewidth=1, zorder=1)
+    y_log[i,:] = x_MC[1]
+
+    unorm_log[i,0] = sum(unorm_MC)
+
+    # ax61.plot(time[0:-1], u_MC, '-', color='lightgrey', linewidth=1, zorder=1)
+
+unorm_avg = np.mean(unorm_log)
+print("avg. of U_norm = %s" %unorm_avg)
+
+output_MC = np.array(output_MC)
+cov_x = np.zeros(num_layers+1)
+cov_y = np.zeros(num_layers+1)
+
+
+# ellipse
+for k in range(num_layers+1):
+    cov_x[k] = np.var(x_log[:,k])
+    cov_y[k] = np.var(y_log[:,k])
+    if k == 0 or k == num_layers:
+        confidence_ellipse(x_log[:, k], y_log[:, k], ax21, edgecolor='blue')
+    # else:
+    #     confidence_ellipse(x_log[:,k], y_log[:,k], ax21, edgecolor='blue')
+
+confidence_ellipse2(np.array([[1,0],[0,1]]), [0,0], ax21, edgecolor='red')
+confidence_ellipse2(np.array([[5,0],[0,5]]), [0,0], ax21, edgecolor='red')
+
+ax22.plot(time, cov_x, '-', color='r', linewidth=2, zorder=2)
+plt.tight_layout()
+ax23.plot(time, cov_y, '-', color='r', linewidth=2, zorder=2)
+plt.tight_layout()
+
+
+# Why not? 3d plot
+# fig4 = plt.figure()
+# ax4 = fig4.add_subplot(111, projection='3d')
+# for i in range(M):
+#     mu0 = np.matrix([[0], [0]])
+#     Sigma0 = np.matrix([[5, 0], [0, 5]])
+#     # x0_MC = mu0 + np.sqrt(Sigma0) * np.random.uniform(low=-1,high=1,size=(2,1))
+#     # x0_MC = [[random.gauss(0, 5)], [random.gauss(0, 5)]]
+#     x0_MC = np.matrix(x[i]).T
+#     x_MC = np.matrix(np.zeros((2, num_layers + 1)))
+#
+#     y_MC = copy.deepcopy(x_MC)
+#     # Uvec = np.matrix(np.zeros((num_layers*2,1)))
+#
+#     for t in range(num_layers + 1):
+#         if t == 0:
+#             x_MC[:, t] = x0_MC
+#         else:
+#             U = K_list_[t - 1] * x_MC[:, t - 1]
+#             Uy = K_list2[t - 1] * x0_MC
+#             x_MC[:, t] = (A + B * K_list_[t - 1]) * x_MC[:, t - 1] + np.matrix(V_list[t - 1]).T
+#             y_MC[:, t] = A * y_MC[:, t - 1] + B * Uy  # + np.matrix(V_list[t]).T
+#
+#         # Uvec[2*t,0] = Uy[0]
+#         # Uvec[2*t+1,0] = Uy[1]
+#     x_MC = np.asarray(x_MC)
+#
+#     # initial point
+#     ax4.scatter3D(x_MC[0][0], 0, x_MC[1][0], color='k', s=2, zorder=2)
+#     # trajectory
+#     ax4.plot3D(x_MC[0], time, x_MC[1], 'gray', linewidth=0.3)
+#     # terminal: These three return the same points
+#     ax4.scatter3D(x_MC[0][num_layers], num_layers, x_MC[1][num_layers], color='r', s=2, zorder=2)
+#     # plt.scatter(y_MC[0,-1], y_MC[1,-1], color='g', s=3)
+#
+#     ax4.scatter3D(x_MC[0][1], 1, x_MC[1][1], color='b', s=1, zorder=2)
+#     ax4.scatter3D(x_MC[0][2], 2, x_MC[1][2], color='b', s=1, zorder=2)
+#     ax4.scatter3D(x_MC[0][3], 3, x_MC[1][3], color='b', s=1, zorder=2)
+#     ax4.scatter3D(x_MC[0][4], 4, x_MC[1][4], color='b', s=1, zorder=2)
+
+plt.show()
+
+
+
---
+++ run_these/planer_trans_wContr.py
@@ -0,0 +1,243 @@
+"""Implementations of planer transforms."""
+
+import numpy as np
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn import init
+
+from nflows.transforms.base import Transform
+from nflows.utils import torchutils
+import nflows.utils.typechecks as check
+
+import global_variable as g
+
+
+class PlanerCache:
+    """Helper class to store the cache of a planer transform.
+
+    The cache consists of: the weight matrix, its inverse and its log absolute determinant.
+    """
+
+    def __init__(self):
+        self.weight = None
+        self.inverse = None
+        self.logabsdet = None
+
+    def invalidate(self):
+        self.weight = None
+        self.inverse = None
+        self.logabsdet = None
+
+
+class Planer(Transform):
+    """Abstract base class for planer transforms that parameterize a weight matrix."""
+
+    def __init__(self, features, using_cache=False):
+        if not check.is_positive_int(features):
+            raise TypeError("Number of features must be a positive integer.")
+        super().__init__()
+
+        self.features = features
+        self.bias = nn.Parameter(torch.zeros(1))
+        self.s = nn.Parameter(torch.rand(1, features))
+
+        # if you want to fix self.bias to zeros
+        # self.bias = torch.zeros(features)
+
+        # Caching flag and values.
+        self.using_cache = using_cache
+        self.cache = PlanerCache()
+
+    def forward(self, inputs, context=None):
+        if not self.training and self.using_cache:
+            self._check_forward_cache()
+            A = torch.tensor([[1, 0.3], [0.5, 1]])
+            B = torch.tensor([[0.7, 0.3], [0.1, 0.8]])
+
+            control = self.s * nn.Tanh(F.linear(torch.matmul(inputs, self.cache.weight), self.bias))
+            outputs = torch.matmul(inputs, A) + torch.matmul(control, B)
+            logabsdet = self.cache.logabsdet * outputs.new_ones(outputs.shape[0])
+            return outputs, logabsdet
+        else:
+            return self.forward_no_cache(inputs)
+
+    def _check_forward_cache(self):
+        if self.cache.weight is None and self.cache.logabsdet is None:
+            self.cache.weight, self.cache.logabsdet = self.weight_and_logabsdet()
+
+        elif self.cache.weight is None:
+            self.cache.weight = self.weight()
+
+        elif self.cache.logabsdet is None:
+            self.cache.logabsdet = self.logabsdet()
+
+    def inverse(self, inputs, context=None):
+        if not self.training and self.using_cache:
+            self._check_inverse_cache()
+            outputs = F.linear(inputs - self.bias, self.cache.inverse)
+            logabsdet = (-self.cache.logabsdet) * outputs.new_ones(outputs.shape[0])
+            return outputs, logabsdet
+        else:
+            return self.inverse_no_cache(inputs)
+
+    def _check_inverse_cache(self):
+        if self.cache.inverse is None and self.cache.logabsdet is None:
+            (
+                self.cache.inverse,
+                self.cache.logabsdet,
+            ) = self.weight_inverse_and_logabsdet()
+
+        elif self.cache.inverse is None:
+            self.cache.inverse = self.weight_inverse()
+
+        elif self.cache.logabsdet is None:
+            self.cache.logabsdet = self.logabsdet()
+
+    def train(self, mode=True):
+        if mode:
+            # If training again, invalidate cache.
+            self.cache.invalidate()
+        return super().train(mode)
+
+    def use_cache(self, mode=True):
+        if not check.is_bool(mode):
+            raise TypeError("Mode must be boolean.")
+        self.using_cache = mode
+
+    def weight_and_logabsdet(self):
+        # To be overridden by subclasses if it is more efficient to compute the weight matrix
+        # and its logabsdet together.
+        return self.weight(), self.logabsdet()
+
+    def weight_inverse_and_logabsdet(self):
+        # To be overridden by subclasses if it is more efficient to compute the weight matrix
+        # inverse and weight matrix logabsdet together.
+        return self.weight_inverse(), self.logabsdet()
+
+    def forward_no_cache(self, inputs):
+        """Applies `forward` method without using the cache."""
+        raise NotImplementedError()
+
+    def inverse_no_cache(self, inputs):
+        """Applies `inverse` method without using the cache."""
+        raise NotImplementedError()
+
+    def weight(self):
+        """Returns the weight matrix."""
+        raise NotImplementedError()
+
+    def weight_inverse(self):
+        """Returns the inverse weight matrix."""
+        raise NotImplementedError()
+
+    def logabsdet(self):
+        """Returns the log absolute determinant of the weight matrix."""
+        raise NotImplementedError()
+
+
+class NaivePlaner(Planer):
+    """A general Planer transform that uses an unconstrained weight matrix."""
+
+    def __init__(self, features, orthogonal_initialization=False, using_cache=False):
+        """Constructor.
+
+        Args:
+            features: int, number of input features.
+            orthogonal_initialization: bool, if True initialize weights to be a random
+                orthogonal matrix.
+
+        Raises:
+            TypeError: if `features` is not a positive integer.
+        """
+        super().__init__(features, using_cache)
+
+        if orthogonal_initialization:
+            self._weight = nn.Parameter(torchutils.random_orthogonal(features))
+        else:
+            self._weight = nn.Parameter(torch.empty(1, features))
+            stdv = 1.0 / np.sqrt(features)
+            init.uniform_(self._weight, -stdv, stdv)
+
+    def forward_no_cache(self, inputs):
+        """Cost:
+            output = O(D^2N)
+            logabsdet = O(D^3)
+        where:
+            D = num of features
+            N = num of inputs
+        """
+        batch_size = inputs.shape[0]
+
+        # define the dynamics
+        A = torch.tensor([[1, 0.3], [0.5, 1]])
+        B = torch.tensor([[0.7, 0.3], [0.1, 0.8]])
+
+        g.alpha0 = 1
+        g.alphaf = 1
+        g.alpha = g.alpha0 + (g.alphaf - g.alpha0) / g.num_iter * g.idx
+
+        control = self.s * torch.tanh(F.linear(inputs, self._weight, self.bias))
+        outputs = torch.matmul(inputs, A) + torch.matmul(control, B)
+
+        a = torch.matmul(self.s, self._weight.T) + self.bias
+        psi = (1 - nn.Tanh()(a) ** 2) * self._weight
+        abs_det = (1 + torch.mm(self.s, psi.T)).abs()
+        logabsdet = torch.log(1e-4 + abs_det)
+        logabsdet = g.alpha * logabsdet * outputs.new_ones(batch_size)  # - torch.norm(control)**2
+        return outputs, torch.flatten(logabsdet)
+
+    def inverse_no_cache(self, inputs):
+        """Cost:
+            output = O(D^3 + D^2N)
+            logabsdet = O(D^3)
+        where:
+            D = num of features
+            N = num of inputs
+        """
+        batch_size = inputs.shape[0]
+        outputs = inputs - self.bias
+        outputs, lu = torch.solve(outputs.t(), self._weight)  # Linear-system solver.
+        outputs = outputs.t()
+        # The linear-system solver returns the LU decomposition of the weights, which we
+        # can use to obtain the log absolute determinant directly.
+        logabsdet = -torch.sum(torch.log(torch.abs(torch.diag(lu))))
+        logabsdet = logabsdet * outputs.new_ones(batch_size)
+        return outputs, logabsdet
+
+    def weight(self):
+        """Cost:
+            weight = O(1)
+        """
+        return self._weight
+
+    def weight_inverse(self):
+        """
+        Cost:
+            inverse = O(D^3)
+        where:
+            D = num of features
+        """
+        return torch.inverse(self._weight)
+
+    def weight_inverse_and_logabsdet(self):
+        """
+        Cost:
+            inverse = O(D^3)
+            logabsdet = O(D)
+        where:
+            D = num of features
+        """
+        # If both weight inverse and logabsdet are needed, it's cheaper to compute both together.
+        identity = torch.eye(self.features, self.features)
+        weight_inv, lu = torch.solve(identity, self._weight)  # Linear-system solver.
+        logabsdet = torch.sum(torch.log(torch.abs(torch.diag(lu))))
+        return weight_inv, logabsdet
+
+    def logabsdet(self):
+        """Cost:
+            logabsdet = O(D^3)
+        where:
+            D = num of features
+        """
+        return torchutils.logabsdet(self._weight)
